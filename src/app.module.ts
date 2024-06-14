40a139cc8c2bce9761a981db586524da62865cc1
Prakhar Singh <prakhar12330@gmail.com>
2024-06-14 11:24:13 +0530

    Our first CRUD API in NestJS

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [BookModule],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule {
+  constructor() {
+    console.log('App Module');
+  }
+}
---
+++ src/book/book.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { BookService } from "./book.service";
+import { Book } from "./data/book.dto";
+
+
+@Controller('/book')
+export class BookController{
+ 
+    constructor(private bookService : BookService){
+        
+    }
+    @Get('/find')
+    getAllBooks():Book[]{
+        return this.bookService.findAllBooksService();
+    }
+    
+    @Put('/update')
+    updateBook(@Body() book:Book):string{
+        return this.bookService.updateBookService(book);
+    }
+    @Post('/add')
+    addBook(@Body() book:Book):string{
+        return this.bookService.addBookService(book);
+    }
+
+    @Delete('/delete/:id')
+    deleteBook(@Param('id') bookId:string):string{
+        return this.bookService.deleteBookService(bookId);
+    }
+}
